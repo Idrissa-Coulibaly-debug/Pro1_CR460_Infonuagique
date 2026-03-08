93a0ee4d751a8ff46c4a0c8ea82af13574a3454a
Idrissa-Coulibaly-debug <hafsetoucoulibaly@gmail.com>
2026-03-08 18:31:50 -0400

    Initial commit Terraform project

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  cloud {
+    organization = "Pipeline_CI_CD_Idrissa_Coulibaly"
+
+    workspaces {
+      name = "Pro1_CR460_Infonuagique"
+    }
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+output "confirmation" {
+  value = "Terraform Cloud est correctement connecté à Azure et GitHub grâce au pipeline CI/CD."
+}
