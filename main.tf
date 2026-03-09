2878148bc74141391ec1ccca892e0c6457699675
Idrissa-Coulibaly-debug <hafsetoucoulibaly@gmail.com>
2026-03-09 17:25:03 -0400

    Q8 - Deploy VNet and subnet

--- main.tf
+++ main.tf
@@ -30,3 +30,22 @@ resource "azurerm_resource_group" "rg_test" {
   name     = "rg-terraform-Idrissa_Coulibaly"
   location = "eastus"
 }
+
+# Déploiement de Réseau Virtuel à partir de votre pipeline dans MS Azure
+
+# VNET
+resource "azurerm_virtual_network" "vnet" {
+  name                = "idrissa-coulibaly-vnet"
+  location            = azurerm_resource_group.rg_test.location
+  resource_group_name = azurerm_resource_group.rg_test.name
+  address_space       = ["10.10.0.0/16"]
+}
+
+# SUBNET
+resource "azurerm_subnet" "subnet_vm" {
+  name                 = "idrissa-coulibaly-subnet-vm"
+  resource_group_name  = azurerm_resource_group.rg_test.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.10.1.0/24"]
+}
+
